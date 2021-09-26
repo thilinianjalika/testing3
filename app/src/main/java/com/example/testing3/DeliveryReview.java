bb9db9e555c3d4d5c44853978e085ade04bc768d
thilinianjalika <brittothilini@gmail.com>
2021-09-26 16:53:32 +0530

    complete order page

--- app/src/main/java/com/example/testing3/DeliveryReview.java
+++ app/src/main/java/com/example/testing3/DeliveryReview.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class DeliveryReview extends AppCompatActivity {
+
+    Buttonbtn_disc
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
